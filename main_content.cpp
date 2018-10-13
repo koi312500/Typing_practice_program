e8c0e2dd3b12e5ea3486d682b0d329ec80489a22
gunwoo7 <b9635005@naver.com>
2018-10-13 13:03:00 +0900

    error-found and change

--- main_content.cpp
+++ main_content.cpp
@@ -39,36 +39,36 @@ int select_main() {
 void word_practice() { // 단어 연습
 	system("cls");
 	PlaySound(TEXT("data/song/main_bgm2.wav"), NULL, SND_ASYNC | SND_LOOP); // main_bgm2.wav 노래 틀기
-	int wordn,randomn,correct = 0,i; // wordn = 파일의 단어 개수, correct = 연습 중 맞은 개수, randomn = 무작위 난수, i = 반복문의 i
+	int wordn, randomn, correct = 0, i; // wordn = 파일의 단어 개수, correct = 연습 중 맞은 개수, randomn = 무작위 난수, i = 반복문의 i
 	double accuracy = 0; // 정확도
 	FILE *wordinput = fopen("data/file/word_practice.dat", "rt"); // 읽기 모드로 파일 열기
 	if (wordinput == NULL) error(); // 파일이 없다면 오류!
 	fscanf(wordinput, "%d", &wordn); // 단어 개수 스캔 받고
-	char word[3005][12],now[12]; // word = 파일에 저장된 모든 단어, now = 현재 입력해야 할 단어
+	char word[3005][12], now[12]; // word = 파일에 저장된 모든 단어, now = 현재 입력해야 할 단어
 
-	for(int i=0;i<wordn;i++){ // 단어의 개수 만큼
+	for (int i = 0; i < wordn; i++) { // 단어의 개수 만큼
 		fscanf(wordinput, "%s", word[i]); // 단어 흡수
 	}
 
 	fclose(wordinput); // 스트림 닫기
 
 	time_t time_now = time(NULL); //시간 측정
-		for (i = 1; i <= 20; i++) {
+	for (i = 1; i <= 20; i++) {
 		randomn = random(wordn - 5); // 무작위 난수를 정한다.
 		system("cls");
-		printf("-------------------- 정확도 : %.1f%% ------------------- 진행률 : %d%% ------------------\n",accuracy,5 * (i-1)); // 출력 화면
+		printf("-------------------- 정확도 : %.1f%% ------------------- 진행률 : %d%% ------------------\n", accuracy, 5 * (i - 1)); // 출력 화면
 		printf("\n\n                                      지금 치실 단어 \n");
 		printf("\n\n------------------------------[                           ]------------------------------\n");
 		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		printf("                                    지금 입력하신 단어\n");
 		printf("\n\n------------------------------[                           ]------------------------------\n\n\n");
 		gotoxy(40, 6);
-		printf("%s",word[randomn]);
+		printf("%s", word[randomn]);
 		gotoxy(40, 26);
 		CursorView(1); // 커서 보이기
-		scanf("%s",now); // 단어 입력
+		scanf("%s", now); // 단어 입력
 		CursorView(0); // 커서 숨기기
-		if(!strcmp(now, word[randomn])) correct += 1; // 맞으면 correct 에 1추가
+		if (!strcmp(now, word[randomn])) correct += 1; // 맞으면 correct 에 1추가
 		accuracy = (double)((double)correct / (double)i) * 100; // 정확도 계산
 	}
 	time_now = time(NULL) - time_now; //시간 측정
@@ -89,11 +89,11 @@ void word_practice() { // 단어 연습
 void short_sentence_practice() {
 	system("cls");
 	PlaySound(TEXT("data/song/main_bgm2.wav"), NULL, SND_ASYNC | SND_LOOP); // main_bgm2.wav 노래 틀기 
-	int wordn = 1261, correct = 0, randomn,i; // wordn = 파일의 단어 개수, correct = 연습 중 맞은 개수, randomn = 무작위 난수, i = 반복문의 i
+	int wordn = 1261, correct = 0, randomn, i; // wordn = 파일의 단어 개수, correct = 연습 중 맞은 개수, randomn = 무작위 난수, i = 반복문의 i
 	double accuracy;
 	char word[3005][100], now[100]; // word = 파일에 저장된 모든 문장, now = 현재 입력해야 할 문장
 	FILE *sentenceinput = fopen("data/file/sentence_practice.dat", "rt"); // 일기 모드로 파일 열기
-	
+
 	if (sentenceinput == NULL) error(); // 파일이 없다면 오류 발생
 
 	for (int i = 0; i < wordn; i++) { // 입력 받고
@@ -107,7 +107,7 @@ void short_sentence_practice() {
 	for (i = 1; i <= 10; i++) {
 		randomn = random(wordn - 5); // 난수 설정
 		system("cls");
-		printf("-------------------- 정확도 : %.1f%% ------------------- 진행률 : %d%% ------------------\n", accuracy, 5 * (i - 1));
+		printf("-------------------- 정확도 : %.1f%% ------------------- 진행률 : %d%% ------------------\n", accuracy, 10 * (i - 1));
 		printf("\n\n                               지금  입력하셔야 하실 문장\n");
 		printf("\n\n-[                                                                                     ]-\n");
 		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
@@ -142,11 +142,13 @@ void Introduction_of_this_program() { // 프로그램 설명
 	Sleep(700);
 	printf("이 프로그램은 대전 정보영재 초급 개인주제 발표대회용 프로그램 입니다.\n");
 	Sleep(700);
-	printf("이 프로그램은 GNU General Public License v3.0 라는 라이센스가 해당됩니다.\n");
+	printf("이 프로그램은 단순명료한 타자연습 프로그램입니다. \n");
 	Sleep(700);
-	printf("이 프로그램의 사용법과 설명에 대한 더 자세한 정보를 알고 싶으시면 개발자 사이트로 오십시오\n");
+	printf("이 프로그램의 사용법과 설명에 대한 더 자세한 정보를\n");
 	Sleep(700);
-	printf("개발자 사이트 : kgw2007.cf\n");
+	printf("알고 싶으시면 단순명료한 타자연습 프로그램 사이트로 오십시오.\n");
+	Sleep(700);
+	printf("단순명료한 타자연습 프로그램 사이트 : kgw2007.cf\n");
 	Sleep(700);
 	printf("이 글은 15초 후에 지워지고 메인화면으로 이동합니다.\n");
 	Sleep(15000);
